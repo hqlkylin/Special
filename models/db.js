6da2835b1f6c5cf1be5775655222192fa9873058
bingjie6699 <duanjuanaccp@126.com>
2015-05-11 13:57:44 +0800

    ip

--- models/db.js
+++ models/db.js
@@ -2,6 +2,6 @@
  * Created by Administrator on 2015/4/27.
  */
 var mongoose=require('mongoose');
-mongoose.connect('mongodb://kylin:123123@localhost:27017/special');
+mongoose.connect('mongodb://kylin:123123@192.163.10.238:27017/special');
 
 module.exports=mongoose;
